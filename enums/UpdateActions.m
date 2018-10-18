fb15535c86d2e2c926a0c8b4dbab93a69d3822ae
Michael Girbino <Michael Girbino@LAPTOP-FOIDLSQO>
2018-10-18 10:40:02 -0400

    Fully implemented 2-way messages between Execution and RegControl for updating shared data.
    Have yet to run simulation to see if I've successfully broken the algebraic loops.

--- enums/UpdateActions.m
+++ enums/UpdateActions.m
@@ -1,7 +1,8 @@
 classdef UpdateActions < uint8
    enumeration
-       PRESENT_TAP  (1)
-       ARMED        (2)
-       HANDLE       (3)
+       PRESENT_TAP          (1)
+       ARMED                (2)
+       HANDLE               (3)
+       PENDING_TAPCHANGE    (4)
    end
 end
