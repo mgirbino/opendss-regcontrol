0c2912dee62cf74f06fc5c08b0b12b05c2ced138
mgirbino <mgirbino@gmail.com>
2018-10-31 09:55:22 -0400

    Saving a copy of QueueAtTime for comparison purposes.

--- experimental/queuetest_sixth_multisim.m
+++ experimental/queuetest_sixth_multisim.m
@@ -5,6 +5,15 @@
 % - Different solver modes and their effect on time
 % - Logging multiple iterations and single iterations of toWorkspace's
 
+LastHandle = Simulink.Parameter;
+LastHandle.DataType = 'double';
+
+TimeInSec = Simulink.Parameter;
+TimeInSec.DataType = 'double';
+
+LastQueue = Simulink.Parameter;
+LastQueue.DataType = 'double';
+
 N = 96;
 simOut = repmat(Simulink.SimulationOutput, N, 1);
 LastQueue.Value = zeros(1,5,50);
@@ -12,33 +21,6 @@ LastHandle.Value = 1;
 
 for nn = 1:N
     tic;
-    % 1 - Obtain power flow from DSS:
-    DSSSolution.SolveNoControl;
-    
-    % 2 - Package DSS measurements for Simulink:
-    DSSCircuit.SetActiveElement('Transformer.TReg1');
-    xfm1 = DSSCircuit.ActiveCktElement;
-
-    ControlledTransformerVoltages.Value = MakeComplex(xfm1.Voltages); 
-    % [in ... | out ...]' (complex)
-
-    ControlledTransformerCurrents.Value = MakeComplex(xfm1.Currents); 
-    % [in ... | out ...]' (complex)
-
-    ControlledTransformerPowers.Value = MakeComplex(xfm1.Powers); 
-    % [in ... | out ...]' (complex)
-
-    DSSCircuit.SetActiveElement('Transformer.TReg1'); % assume this is at the regulated bus
-    RegulatedBus = DSSCircuit.ActiveCktElement;
-
-    VTerminal.Value = MakeComplex(RegulatedBus.Voltages); 
-    % [in ... | out ...]' (complex)
-    
-    xfms = DSSCircuit.Transformers;
-    xfms.Name = 'TReg1';
-    TReg1.Winding(tw).puTap = double(xfms.Tap);
-    
-    PresentTap.Value = double(TReg1.Winding(tw).puTap); 
     
     % 3 - configure simulation parameters with prior timestep's results:    
     TimeInSec.Value = double( nn*(24/N)*3600 );
@@ -51,16 +33,13 @@ for nn = 1:N
     end
     
     % 4 - obtain control actions from Simulink:    
-    simOut(nn) = sim('regcontrol_model', 'timeout', 1000);
+    simOut(nn) = sim('queuetest_sixth', 'timeout', 1000);
     
-    % 5 - execute tap changes in DSS:
-%     xfms.Tap = xfms.Tap + TapChangeToMake.Data(end);
-    xfms.Tap = xfms.Tap + TapChangeToMake;
-    
-    TimeElapsed = toc;
-    
+    TimeElapsed = toc;    
     fprintf('Iteration %d, Time = %g\n', nn, TimeElapsed);
-    
-    % 5 - Power flow after control actions:    
-    DSSSolution.Solve;
 end
+
+
+
+
+
